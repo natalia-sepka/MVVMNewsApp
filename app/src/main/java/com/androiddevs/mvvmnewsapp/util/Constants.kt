4419a72c19a559d19872db6cfed925fa7cf00f75
natalia.sepka <natalia.sepka@gmail.com>
2023-07-06 16:33:17 +0200

    Searching news

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "df72981fcab34cb9b35d4a4da7eced1b"
         const val BASE_URL = "https:newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
