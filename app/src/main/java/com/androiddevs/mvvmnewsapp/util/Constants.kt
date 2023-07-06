431dedac552163865c30a70aa317ef154d5dc1c1
natalia.sepka <natalia.sepka@gmail.com>
2023-07-06 09:27:57 +0200

    Retrofit setup

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "df72981fcab34cb9b35d4a4da7eced1b"
+        const val BASE_URL = "https:newsapi.org"
+    }
+}
